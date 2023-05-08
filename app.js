3af7c30ed635d2e1abf2afb3461acaaffbacd696
Kalina Frontend <dzhiltcov@yandex.ru>
2023-05-08 10:57:18 +0200

    feat: создать app файл и установить зависимости

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+ const { PORT = 3000 } = process.env;
+
+ const app = express();
+ app.use(express.json());
+
+mongoose.set('strictQuery', false);
+ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+ app.listen(PORT, () => {
+  console.log (`App listening on port ${PORT}`);
+ })
